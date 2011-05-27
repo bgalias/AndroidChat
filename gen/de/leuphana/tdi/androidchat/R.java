a21de92791fdc1ceb78731fa843f19f5a321d55d
bgalias <bgalias@trash-mail.com>
2011-05-27 21:40:34 +0200

    Refactoring

--- gen/de/leuphana/tdi/androidchat/R.java
+++ gen/de/leuphana/tdi/androidchat/R.java
@@ -14,7 +14,7 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int editText1=0x7f050000;
+        public static final int button1=0x7f050000;
     }
     public static final class layout {
         public static final int main=0x7f030000;
--- src/de/leuphana/tdi/androidchat/AndroidChat.java
+++ src/de/leuphana/tdi/androidchat/AndroidChat.java
@@ -4,48 +4,53 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class AndroidChat extends Activity {
-	
+
 	private AsyncTask<Integer, String, String> chatClient;
 	private Integer port;
-	
-    /** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-        
-        chatClient = new ChatClient().execute(port);
-    }
-    
-    private class ChatClient extends AsyncTask<Integer, String, String>{
+
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		// chatClient = new ChatClient().execute(port);
+	}
+
+	private class ChatClient extends AsyncTask<Integer, String, String> {
 
 		@Override
 		protected String doInBackground(Integer... params) {
 			try {
 				DatagramSocket clientSocket = new DatagramSocket(params[0]);
 				DatagramPacket packet = new DatagramPacket(new byte[512], 512);
-				
-				while(true){
+
+				while (true) {
 					clientSocket.receive(packet);
-					publishProgress(new String(packet.getData(),0,packet.getLength()));
+					publishProgress(new String(packet.getData(), 0,
+							packet.getLength()));
 				}
 			} catch (Exception e) {
 			}
 			return null;
 		}
-    	
+
 		@Override
-		protected void onProgressUpdate(String... params){
-			
+		protected void onProgressUpdate(String... params) {
+
 		}
-		
+
 		@Override
-		protected void onPostExecute(String result){
-			
+		protected void onPostExecute(String result) {
+
 		}
-    }
+	}
 }
