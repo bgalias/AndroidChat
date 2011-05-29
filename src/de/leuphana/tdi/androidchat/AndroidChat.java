ba05fbb53f03ecc3534c0b8ebd82a01c73857090
bgalias <bgalias@trash-mail.com>
2011-05-29 18:39:54 +0200

    Refactored ChatClient and Added Preferences-UI

--- src/de/leuphana/tdi/androidchat/AndroidChat.java
+++ src/de/leuphana/tdi/androidchat/AndroidChat.java
@@ -1,20 +1,9 @@
 package de.leuphana.tdi.androidchat;
 
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-
 import android.app.Activity;
-import android.content.Intent;
-import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.widget.Button;
 
 public class AndroidChat extends Activity {
-
-	private AsyncTask<Integer, String, String> chatClient;
-	private Integer port;
 
 	/** Called when the activity is first created. */
 	@Override
@@ -22,35 +11,5 @@ public class AndroidChat extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main);
 
-		// chatClient = new ChatClient().execute(port);
-	}
-
-	private class ChatClient extends AsyncTask<Integer, String, String> {
-
-		@Override
-		protected String doInBackground(Integer... params) {
-			try {
-				DatagramSocket clientSocket = new DatagramSocket(params[0]);
-				DatagramPacket packet = new DatagramPacket(new byte[512], 512);
-
-				while (true) {
-					clientSocket.receive(packet);
-					publishProgress(new String(packet.getData(), 0,
-							packet.getLength()));
-				}
-			} catch (Exception e) {
-			}
-			return null;
-		}
-
-		@Override
-		protected void onProgressUpdate(String... params) {
-
-		}
-
-		@Override
-		protected void onPostExecute(String result) {
-
-		}
 	}
 }
